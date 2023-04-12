fc15f8afc20d28082c4db8d4d237c2119916899e
Fahad <af4x305@gmail.com>
2023-04-12 05:12:44 +0300

    the tools used by hunter

---
+++ tools/shosubgo-2.0/README.md
@@ -0,0 +1,24 @@
+# shosubgo
+Small tool to Grab subdomains using Shodan api.
+
+## Get your shodan api FREE with limit usage:
+<https://developer.shodan.io/api/requirements>
+
+## Usage
+```bash
+go run main.go -d target.com -s YourAPIKEY
+```
+## Usage download from releases:
+
+https://github.com/b4ruiO/shosubgo/releases/tag/1.1
+
+```bash
+# From Download Releases
+
+./shosubgo_linux -d target.com -s YourAPIKEY
+```
+
+![shosubgo](https://raw.githubusercontent.com/incogbyte/shosubgo/master/shosubgo.png)
+
+
+![gopher](https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTFcFPxQzLnq18PnHBkUxF6KfavmHX9q6Ukz-JWSNOg7iJu7Dsy)
---
+++ tools/shosubgo-2.0/main.go
@@ -0,0 +1,72 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"os"
+
+	"apishodan"
+)
+
+const Author = "0xrod"
+
+func main() {
+	domain := flag.String("d", "", "[+] Domain to find subdomains")
+	shodanKey := flag.String("s", "", "[+] Shodan api key")
+	verbose := flag.Bool("v", false, "[+] Show all output")
+	fileName := flag.String("o", "", "[+] Save domains into a file")
+	flag.Parse()
+
+	if *domain == "" || *shodanKey == "" {
+		fmt.Printf("[*] Usage %s -d target.com -s MYShodaNKey", os.Args[0])
+		fmt.Printf("[*] Author: %s \n\n thanks for using this tool =) PR are welcome", Author)
+		os.Exit(1)
+	}
+
+	apiKey := apishodan.New(*shodanKey)
+
+	domainSearch := *domain
+
+	subdomain, err := apiKey.GetSubdomain(domainSearch)
+	if err != nil {
+		log.Panicln(err)
+	}
+
+	if *verbose == true {
+
+		info, err := apiKey.InfoAccount()
+
+		if err != nil {
+			log.Panicln(err)
+		}
+
+		fmt.Printf(
+			"[*] Credits: %d\nScan Credits: %d \n\n",
+			info.QueryCredits, info.ScanCredits)
+
+		for _, v := range subdomain.Data {
+			d := v.SubD + subdomain.Domain
+			fmt.Printf("[*] Domain: %s\nIP/DNS :%s\nLast Scan made by shodan:%s\n", d, v.Value, v.LastSeen)
+		}
+
+	} else {
+		for _, v := range subdomain.SubDomains {
+			if *fileName != "" {
+				f, err := os.Create(*fileName)
+				if err != nil {
+					log.Fatal(err)
+				}
+
+				defer f.Close()
+
+				_, err2 := f.WriteString(v + "\n")
+				if err2 != nil {
+					log.Fatal(err2)
+				}
+				fmt.Println("[*] DONE write files")
+			}
+			fmt.Printf("%s.%s\n", v, domainSearch)
+		}
+	}
+}
